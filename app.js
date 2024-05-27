bf685c94757497ead16b78594c44e5633fd74056
Dimitris C. K <dimchatzkar@gmail.com>
2024-05-28 02:11:38 +0300

    Refactors, clean-ups and added the generate Code page

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ db.connect((error) => {
 app.set('view engine', 'hbs')
 
 // other imports
-const path = require("path")
+const path = require("path");
+const { randomUUID } = require('crypto');
 
 const publicDir = path.join(__dirname, './public')
 
@@ -40,29 +41,62 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get('/code/:promolink', (req, res) => {
+app.get("/generateCode", (req, res) => {
+    res.render("generateCode", { promocode: "0000-0000-0000-0000" })
+})
+
+
+app.post("/promocode/generate", (req, res) => {
+    const promoCodeID = randomUUID();
+    const promoLink = randomUUID();
+    const expDate = setExpDate(1, 0, 0);
+
+    db.query(
+        'INSERT INTO promocodes (ID, Number, ExpireDate, Active) VALUES (?, ?, ?, ?)',
+        [promoCodeID, randomUUID(), expDate, 1],
+        async (error, ressc) => {
+
+            if (error) {
+                console.log(error)
+                res.render('promocode', { promocode: "Database Error while creating promotion code ERR:PG1" });
+            }
+            db.query(
+                'INSERT INTO promotions(ID, link, CodeID, UnlockKey) VALUES (?, ?, ?, ?)',
+                [randomUUID(), promoLink, promoCodeID, randomUUID()],
+                async (error, ressp) => {
+
+                    if (error) {
+                        console.log(error)
+                        res.render('promocode', { promocode: "Database Error while creating promotion. ERR:PG2" });
+                    }
+                    res.render("generateCode", { promocode: promoLink })
+                })
+        })
+})
+
+
+app.get('/promocode/:promolink', (req, res) => {
     const promoLink = req.params.promolink;
 
     db.query(
-        'SELECT * FROM promocodes INNER JOIN codes on codes.ID = promocodes.CodeID where link = ?',
+        'SELECT * FROM promotions INNER JOIN promocodes on promocodes.ID = promotions.CodeID where link = ?',
         [promoLink],
         async (error, ress) => {
             if (error) {
                 console.log(error)
-                res.render('code', { promocode: "Database Error" });
+                res.render('promocode', { promocode: "Database Error" });
             } else {
                 if (ress != undefined && ress[0] != undefined) { // has rows
                     dbRow = ress[0]
 
                     if (dbRow.Active) {
-                        console.log(ress)
                         const dbpromoCode = ress[0].Number;
-                        res.render('code', { promocode: dbpromoCode });
+                        res.render('promocode', { promocode: dbpromoCode });
                     } else {
-                        res.render('code', { promocode: "", backError: "Inactive Promo" });
+                        res.render('promocode', { promocode: "", backError: "Inactive Promo" });
                     }
                 } else {
-                    res.render('code', { promocode: "", backError: "Invalid Link" });
+                    res.render('promocode', { promocode: "", backError: "Invalid Link" });
                 }
             }
         })
@@ -74,3 +108,12 @@ app.get('/code/:promolink', (req, res) => {
 app.listen(5000, () => {
     console.log("server started on port 5000")
 })
+
+function setExpDate(expYears, expMonths, expDays) {
+    var expDate = new Date();
+
+    expDate.setFullYear(expDate.getFullYear() + expYears);
+    expDate.setMonth(expDate.getMonth() + expMonths);
+    expDate.setDate(expDate.getDate() + expDays);
+    return expDate;
+}
